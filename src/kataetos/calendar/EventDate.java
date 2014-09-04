68a26612a573f163944ef90b687f60bf21a9409f
Peter Mansour <pmansour@student.unimelb.edu.au>
2014-09-04 15:14:33 +1000

    started writing the skeleton for the first class.

---
+++ src/kataetos/calendar/EventDate.java
@@ -0,0 +1,11 @@
+package kataetos.calendar;
+
+/**
+ * A date on which an event occurs. These dates are not tied to any particular year, but can be
+ * made concrete given a calendar year.
+ * 
+ * @author peter
+ */
+public abstract class EventDate {
+
+}
